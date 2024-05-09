a65dbe0053260111cb93429b86b9bf07526a3b4d
Fawang <fwcheung@outlook.com>
2024-05-09 16:28:13 +0800

    [uFTB]: flatten uFTB module

--- src/main/scala/xiangshan/frontend/FauFTB.scala
+++ src/main/scala/xiangshan/frontend/FauFTB.scala
@@ -53,7 +53,6 @@ class FauFTBWay(implicit p: Parameters) extends XSModule with FauFTBParams {
   private val tag   = Reg(UInt(tagSize.W))
   private val data  = Reg(new FTBEntry)
 
-
   io.resp := data
   io.respHit := tag === io.reqTag && valid
 
@@ -68,58 +67,11 @@ class FauFTBWay(implicit p: Parameters) extends XSModule with FauFTBParams {
   }
 }
 
-class FauFTBBank(implicit p: Parameters) extends XSModule with FauFTBParams {
-  val io = IO(new Bundle {
-    val reqTag = Input(UInt(tagSize.W))
-    val respHitOH = Output(Vec(numWays, Bool()))
-    val respEntries = Output(Vec(numWays, new FTBEntry))
-    val respCtrs = Output(Vec(numWays, UInt(2.W)))
-
-    val updateReqTag = Input(UInt(tagSize.W))
-    val updateHitOH = Output(Vec(numWays, Bool()))
-
-    val writeValidOH = Input(Vec(numWays, Bool()))
-    val writeTag = Input(UInt(tagSize.W))
-    val writeEntry = Input(new FTBEntry)
-
-    val writeCtrsValid = Input(Vec(numWays, Bool()))
-    val writeCtrs = Input(Vec(numWays, UInt(2.W)))
-  })
-
-  private val ways = Seq.fill(numWays)(Module(new FauFTBWay))
-  private val ctrs = Seq.fill(numWays)(RegInit(2.U(2.W)))
-
-  /** Predict Require */
-  ways.foreach(_.io.reqTag := io.reqTag)
-
-  /** Predict Response */
-  io.respHitOH := ways.map(_.io.respHit)
-  io.respEntries := ways.map(_.io.resp)
-  io.respCtrs := ctrs
-
-  /** Update Require */
-  ways.foreach(_.io.updateReqTag := io.updateReqTag)
-  io.updateHitOH := VecInit(ways.map(_.io.updateHit))
-
-  /** Update write tag and entry */
-  ways.zip(io.writeValidOH).foreach { case (w, v) => w.io.writeValid := v }
-  ways.foreach(_.io.writeTag := io.writeTag)
-  ways.foreach(_.io.writeEntry := io.writeEntry)
-
-  /** Update write counter */
-  for (ctr & valid & writeCtr <- ctrs zip io.writeCtrsValid zip io.writeCtrs) {
-    when(valid) {
-      ctr := writeCtr
-    }
-  }
-}
-
 class FauFTB(implicit p: Parameters) extends BasePredictor with FauFTBParams {
 
   private val fauftbEnable = RegNext(dup(io.ctrl.ubtb_enable))
 
   class FauFTBMeta(implicit p: Parameters) extends XSBundle with FauFTBParams {
-    //val predWay = UInt(log2Ceil(numWays).W)
     val pred_way = if (!env.FPGAPlatform) Some(UInt(log2Ceil(numWays).W)) else None
     val hit = Bool()
   }
@@ -127,35 +79,36 @@ class FauFTB(implicit p: Parameters) extends BasePredictor with FauFTBParams {
   override val meta_size = respMeta.getWidth
   override val is_fast_pred = true
 
-  
-  private val banks = Module(new FauFTBBank)
-  dontTouch(banks.io)
+  private val ways = Seq.fill(numWays)(Module(new FauFTBWay))
+  private val ctrs = Seq.fill(numWays)(RegInit(2.U(2.W)))
   
   private val replacer = ReplacementPolicy.fromString("plru", numWays)
   private val replacerTouchWays = Wire(Vec(2, Valid(UInt(log2Ceil(numWays).W))))
-
-  private val s1_fire = io.s1_fire(dupForUbtb)
-  private val s1_pc = s1_pc_dup(dupForUbtb)
 
   /** Prediction
    * FauFtb's bank prediction require by pc only,
    * response all entries, hit one-hot and counters
    * which construct the stage one of [[FullBranchPrediction]].
    */
-  banks.io.reqTag := getTag(s1_pc_dup(dupForUbtb))
 
-  val s1_hitOH: UInt = banks.io.respHitOH.asUInt
-  val s1_hit: Bool = s1_hitOH.orR
-  val s1_hitWay: UInt = OHToUInt(s1_hitOH)
-  val s1_predCandidate: Vec[FullBranchPrediction] = Wire(Vec(numWays, new FullBranchPrediction))
-  
-  banks.io.respCtrs zip banks.io.respEntries zip s1_predCandidate foreach { case((ctr, entry), pred) =>
+  private val s1_fire = io.s1_fire(dupForUbtb)
+  private val s1_pc = s1_pc_dup(dupForUbtb)
+
+  ways.foreach(_.io.reqTag := getTag(s1_pc))
+
+  private val s1_entry = ways.map(_.io.resp)
+  private val s1_hitOH: UInt = VecInit(ways.map(_.io.respHit)).asUInt
+  private val s1_hit: Bool = s1_hitOH.orR
+  private val s1_hitWay: UInt = OHToUInt(s1_hitOH)
+  private val s1_predCandidate: Vec[FullBranchPrediction] = Wire(Vec(numWays, new FullBranchPrediction))
+
+  ctrs zip s1_entry zip s1_predCandidate foreach { case((ctr, entry), pred) =>
     pred.hit := DontCare
     pred.br_taken := ctr(1) || entry.alwaysTaken
     pred.fromFtbEntry(entry, s1_pc)
   }
 
-  val s1_hitPred: FullBranchPrediction = Mux1H(s1_hitOH, s1_predCandidate)
+  private val s1_hitPred: FullBranchPrediction = Mux1H(s1_hitOH, s1_predCandidate)
   
   XSError(PopCount(s1_hitOH) > 1.U, "fauFtb has multiple hits!\n")
   
@@ -163,11 +116,10 @@ class FauFTB(implicit p: Parameters) extends BasePredictor with FauFTBParams {
   io.out.s1.fullPred.zip(fauftbEnable).foreach {case (fp, en) => fp.hit := s1_hit && en}
 
   respMeta.hit := RegEnable(RegEnable(s1_hit, io.s1_fire(dupForUbtb)), io.s2_fire(dupForUbtb))
-  //respMeta.predWay := RegEnable(RegEnable(s1_hitWay, io.s1_fire(dupForUbtb)), io.s2_fire(dupForUbtb))
   if(respMeta.pred_way.isDefined) {respMeta.pred_way.get := RegEnable(RegEnable(s1_hitWay, io.s1_fire(0)), io.s2_fire(0))}
   io.out.lastStageMeta := respMeta.asUInt
 
-  private val s1_ftbEntry = Mux1H(s1_hitOH, banks.io.respEntries)
+  private val s1_ftbEntry = Mux1H(s1_hitOH, s1_entry)
   io.out.lastStageFtbEntry := RegEnable(RegEnable(s1_ftbEntry, io.s1_fire(dupForUbtb)), io.s2_fire(dupForUbtb))
 
   for (i <- 1 until numDup) {
@@ -186,8 +138,8 @@ class FauFTB(implicit p: Parameters) extends BasePredictor with FauFTBParams {
   private val u_meta = us.bits.meta.asTypeOf(new FauFTBMeta)
   private val u_s0_tag = getTag(us.bits.pc)
 
-  banks.io.updateReqTag := u_s0_tag
-  private val u_s0_hitOH = banks.io.updateHitOH.asUInt
+  ways.foreach(_.io.updateReqTag := u_s0_tag)
+  private val u_s0_hitOH = VecInit(ways.map(_.io.updateHit)).asUInt
   private val u_s0_hit = u_s0_hitOH.orR
   private val u_s0_brUpdateValid = us.bits.ftbEntry.brValid && us.valid && !us.bits.ftbEntry.alwaysTaken
 
@@ -204,13 +156,16 @@ class FauFTB(implicit p: Parameters) extends BasePredictor with FauFTBParams {
   private val u_s1_writeWayOH = Mux(u_s1_hit, u_s1_hitOH, UIntToOH(u_s1_allocWay))
   private val u_s1_writeValidOH = u_s1_writeWayOH.asBools.zipWithIndex.map{ case (way, i) => way && u_s1_valid(i) }
 
-  for (w <- 0 until numWays) {
-    banks.io.writeValidOH(w)  := u_s1_writeValidOH(w)
-    banks.io.writeTag         := u_s1_tag
-    banks.io.writeEntry       := u_s1_ftbEntry
+  ways.zip(u_s1_writeValidOH).foreach{ case (w,v) => w.io.writeValid := v }
+  ways.foreach(_.io.writeTag := u_s1_tag)
+  ways.foreach(_.io.writeEntry := u_s1_ftbEntry)
 
-    banks.io.writeCtrsValid(w) := u_s1_brUpdateValid && u_s1_writeValidOH(w)
-    banks.io.writeCtrs(w)      := satUpdate(banks.io.respCtrs(w), len = 2, u_s1_brTaken)
+  when(u_s1_brUpdateValid){
+    ctrs.zip(u_s1_writeValidOH).foreach { case (ctr, v) =>
+      when(v) {
+        ctr := satUpdate(ctr, len = 2, u_s1_brTaken)
+      }
+    }
   }
 
   /** Replacer
@@ -226,7 +181,6 @@ class FauFTB(implicit p: Parameters) extends BasePredictor with FauFTBParams {
   /** Performance counters */
   val s0_fire_next_cycle = RegNext(io.s0_fire(dupForUbtb))
   val u_pred_hit_way_map   = (0 until numWays).map(w => s0_fire_next_cycle && s1_hit && s1_hitWay === w.U)
-  //val u_commit_hit_way_map = (0 until numWays).map(w => us.valid && u_meta.hit && u_meta.predWay === w.U)
   if(u_meta.pred_way.isDefined) {
     val u_commit_hit_way_map = (0 until numWays).map(w => us.valid && u_meta.hit && u_meta.pred_way.get === w.U)
     for (w <- 0 until numWays) {
