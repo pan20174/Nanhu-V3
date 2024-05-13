17c4332b548e79107c47f55c49190b33e4a515de
Jinghui-Z <771982360@qq.com>
2024-05-13 11:27:08 +0800

    Fallback IBuffer version

--- src/main/scala/xiangshan/frontend/IBuffer.scala
+++ src/main/scala/xiangshan/frontend/IBuffer.scala
@@ -25,19 +25,9 @@ import xs.utils._
 import xiangshan.ExceptionNO._
 import xs.utils.perf.HasPerfLogging
 
-class IBufPtr(implicit p: Parameters) extends CircularQueuePtr[IBufPtr](
+class IbufPtr(implicit p: Parameters) extends CircularQueuePtr[IbufPtr](
   p => p(XSCoreParamsKey).IBufSize
-) {
-}
-
-class IBufInBankPtr(implicit p: Parameters) extends CircularQueuePtr[IBufInBankPtr](
-  p => p(XSCoreParamsKey).IBufSize / p(XSCoreParamsKey).IBufNBank
-) {
-}
-
-class IBufBankPtr(implicit p: Parameters) extends CircularQueuePtr[IBufBankPtr](
-  p => p(XSCoreParamsKey).IBufNBank
-) {
+){
 }
 
 class IBufferIO(implicit p: Parameters) extends XSBundle {
@@ -101,193 +91,82 @@ class IBufEntry(implicit p: Parameters) extends XSBundle {
 class IBuffer(implicit p: Parameters) extends XSModule with HasCircularQueuePtrHelper with HasPerfEvents with HasPerfLogging {
   val io = IO(new IBufferIO)
 
-  // Parameter Check
-  private val bankSize = IBufSize / IBufNBank
-  require(IBufSize % IBufNBank == 0, s"IBufNBank should divide IBufSize, IBufNBank: $IBufNBank, IBufSize: $IBufSize")
-  require(IBufNBank >= DecodeWidth,
-    s"IBufNBank should be equal or larger than DecodeWidth, IBufNBank: $IBufNBank, DecodeWidth: $DecodeWidth")
+  val ibuf = Module(new SyncDataModuleTemplate(new IBufEntry, IBufSize, 2 * DecodeWidth, PredictWidth, "IBuffer"))
 
-  // IBuffer is organized as raw registers
-  // This is due to IBuffer is a huge queue, read & write port logic should be precisely controlled
-  //                             . + + E E E - .
-  //                             . + + E E E - .
-  //                             . . + E E E - .
-  //                             . . + E E E E -
-  // As shown above, + means enqueue, - means dequeue, E is current content
-  // When dequeue, read port is organized like a banked FIFO
-  // Dequeue reads no more than 1 entry from each bank sequentially, this can be exploit to reduce area
-  // Enqueue writes cannot benefit from this characteristic unless use a SRAM
-  // For detail see Enqueue and Dequeue below
-  private val ibuf: Vec[IBufEntry] = RegInit(VecInit.fill(IBufSize)(0.U.asTypeOf(new IBufEntry)))
-  private val bankedIBufView: Vec[Vec[IBufEntry]] = VecInit.tabulate(IBufNBank)(
-    bankID => VecInit.tabulate(bankSize)(
-      inBankOffset => ibuf(bankID + inBankOffset * IBufNBank)
-    )
-  )
-  // Bypass wire
-  private val bypassEntries = WireDefault(VecInit.fill(DecodeWidth)(0.U.asTypeOf(Valid(new IBufEntry))))
-  // Normal read wire
-  private val deqEntries = WireDefault(VecInit.fill(DecodeWidth)(0.U.asTypeOf(Valid(new IBufEntry))))
-  // Output register
-  private val outputEntries = RegInit(VecInit.fill(DecodeWidth)(0.U.asTypeOf(Valid(new IBufEntry))))
+  val deqPtrVec = RegInit(VecInit.tabulate(2 * DecodeWidth)(_.U.asTypeOf(new IbufPtr)))
+  val deqPtrVecNext = Wire(Vec(2 * DecodeWidth, new IbufPtr))
+  deqPtrVec := deqPtrVecNext
+  val deqPtr = deqPtrVec(0)
 
-  // Between Bank
-  private val deqBankPtrVec: Vec[IBufBankPtr] = RegInit(VecInit.tabulate(DecodeWidth)(_.U.asTypeOf(new IBufBankPtr)))
-  private val deqBankPtr: IBufBankPtr = deqBankPtrVec(0)
-  // Inside Bank
-  private val deqInBankPtr: Vec[IBufInBankPtr] = RegInit(VecInit.fill(IBufNBank)(0.U.asTypeOf(new IBufInBankPtr)))
-
-  val deqPtr = RegInit(0.U.asTypeOf(new IBufPtr))
-
-  val enqPtrVec = RegInit(VecInit.tabulate(PredictWidth)(_.U.asTypeOf(new IBufPtr)))
+  val enqPtrVec = RegInit(VecInit.tabulate(PredictWidth)(_.U.asTypeOf(new IbufPtr)))
   val enqPtr = enqPtrVec(0)
 
   val validEntries = distanceBetween(enqPtr, deqPtr)
   val allowEnq = RegInit(true.B)
-  val useBypass = enqPtr === deqPtr && io.out.head.ready // empty and last cycle fire
 
-  val numFromFetch = PopCount(io.in.bits.enqEnable)
-  val numTryEnq = WireDefault(0.U)
-  val numEnq = Mux(io.in.fire, numTryEnq, 0.U)
-  val numBypass = PopCount(bypassEntries.map(_.valid))
+  val numEnq = Mux(io.in.fire, PopCount(io.in.bits.valid), 0.U)
   val numTryDeq = Mux(validEntries >= DecodeWidth.U, DecodeWidth.U, validEntries)
   val numDeq = Mux(io.out.head.ready, numTryDeq, 0.U)
+  deqPtrVecNext := Mux(io.out.head.ready, VecInit(deqPtrVec.map(_ + numTryDeq)), deqPtrVec)
+
   val numAfterEnq = validEntries +& numEnq
-
   val nextValidEntries = Mux(io.out(0).ready, numAfterEnq - numTryDeq, numAfterEnq)
-  allowEnq := (IBufSize - PredictWidth).U >= nextValidEntries // Disable when almost full
+  allowEnq := (IBufSize - PredictWidth).U >= nextValidEntries
 
-  val enqOffset = VecInit.tabulate(PredictWidth)(i => PopCount(io.in.bits.valid.asBools.take(i)))
-  val enqData = VecInit.tabulate(PredictWidth)(i => Wire(new IBufEntry).fromFetch(io.in.bits, i))
-
-  // when using bypass, bypassed entries do not enqueue
-  when(useBypass) {
-    when(numFromFetch >= DecodeWidth.U) {
-      numTryEnq := numFromFetch - DecodeWidth.U
-    } .otherwise {
-      numTryEnq := 0.U
-    }
-  } .otherwise {
-    numTryEnq := numFromFetch
-  }
-
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  // Bypass
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  bypassEntries.zipWithIndex.foreach {
-    case (entry, idx) =>
-      // Select
-      val validOH = Range(0, PredictWidth).map {
-        i =>
-          io.in.bits.valid(i) &&
-            io.in.bits.enqEnable(i) &&
-            enqOffset(i) === idx.asUInt
-      } // Should be OneHot
-      entry.valid := validOH.reduce(_ || _) && io.in.fire && !io.flush
-      entry.bits := Mux1H(validOH, enqData)
-
-      // Debug Assertion
-      XSError(PopCount(validOH) > 1.asUInt, "validOH is not OneHot")
-  }
-
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  // Out Logic
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  // => Decode Output
-  // clean register output
-  io.out zip outputEntries foreach {
-    case (io, reg) =>
-      io.valid := reg.valid
-      io.bits := reg.bits.toCtrlFlow
-  }
-  outputEntries zip bypassEntries zip deqEntries foreach {
-    case ((out, bypass), deq) =>
-      when(io.out.head.ready) {
-        out := deq
-        when(useBypass) {
-          out := bypass
-        }
-      }
-  }
-
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  // Enqueue
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Enque
   io.in.ready := allowEnq
-  // Data
-  ibuf.zipWithIndex.foreach {
-    case (entry, idx) => {
-      // Select
-      val validOH = Range(0, PredictWidth).map {
-        i =>
-          val useBypassMatch = enqOffset(i) >= DecodeWidth.U &&
-            enqPtrVec(enqOffset(i) - DecodeWidth.U).value === idx.asUInt
-          val normalMatch = enqPtrVec(enqOffset(i)).value === idx.asUInt
-          val matchRes = Mux(useBypass, useBypassMatch, normalMatch) // when using bypass, bypassed entries do not enqueue
 
-          io.in.bits.valid(i) && io.in.bits.enqEnable(i) && matchRes
-      } // Should be OneHot
-      val wen = validOH.reduce(_ || _) && io.in.fire && !io.flush
-
-      // Write port
-      // Each IBuffer entry has a PredictWidth -> 1 Mux
-      val writeEntry = Mux1H(validOH, enqData)
-      entry := Mux(wen, writeEntry, entry)
-
-      // Debug Assertion
-      XSError(PopCount(validOH) > 1.asUInt, "validOH is not OneHot")
-    }
+  val enqOffset = Seq.tabulate(PredictWidth)(i => PopCount(io.in.bits.valid.asBools.take(i)))
+  val enqData = Seq.tabulate(PredictWidth)(i => Wire(new IBufEntry).fromFetch(io.in.bits, i))
+  for (i <- 0 until PredictWidth) {
+    ibuf.io.waddr(i) := enqPtrVec(enqOffset(i)).value
+    ibuf.io.wdata(i) := enqData(i)
+    ibuf.io.wen(i)   := io.in.bits.enqEnable(i) && io.in.fire && !io.flush
   }
-  // Pointer maintenance
+
   when (io.in.fire && !io.flush) {
-    enqPtrVec := VecInit(enqPtrVec.map(_ + numTryEnq))
+    enqPtrVec := VecInit(enqPtrVec.map(_ + PopCount(io.in.bits.enqEnable)))
   }
 
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // Dequeue
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   val validVec = Mux(validEntries >= DecodeWidth.U,
     ((1 << DecodeWidth) - 1).U,
     UIntToMask(validEntries(log2Ceil(DecodeWidth) - 1, 0), DecodeWidth)
   )
-  // Data
-  // Read port
-  // 2-stage, IBufNBank * (bankSize -> 1) + IBufNBank -> 1
-  // Should be better than IBufSize -> 1 in area, with no significant latency increase
-  private val readStage1: Vec[IBufEntry] = VecInit.tabulate(IBufNBank)(
-    bankID => Mux1H(UIntToOH(deqInBankPtr(bankID).value), bankedIBufView(bankID))
-  )
+  val deqData = Reg(Vec(DecodeWidth, new IBufEntry))
   for (i <- 0 until DecodeWidth) {
-    deqEntries(i).valid := validVec(i)
-    deqEntries(i).bits := Mux1H(UIntToOH(deqBankPtrVec(i).value), readStage1)
+    io.out(i).valid := validVec(i)
+    // by default, all bits are from the data module (slow path)
+    io.out(i).bits := ibuf.io.rdata(i).toCtrlFlow
+    // some critical bits are from the fast path
+    val fastData = deqData(i).toCtrlFlow
+    io.out(i).bits.instr := fastData.instr
+    io.out(i).bits.exceptionVec := fastData.exceptionVec
+    io.out(i).bits.foldpc := fastData.foldpc
+    XSError(io.out(i).fire && fastData.instr =/= ibuf.io.rdata(i).toCtrlFlow.instr, "fast data error\n")
   }
-  // Pointer maintenance
-  deqBankPtrVec := Mux(io.out.head.ready, VecInit(deqBankPtrVec.map(_ + numTryDeq)), deqBankPtrVec)
-  deqPtr := Mux(io.out.head.ready, deqPtr + numTryDeq, deqPtr)
-  deqInBankPtr.zipWithIndex.foreach {
-    case (ptr, idx) => {
-      // validVec[k] == bankValid[deqBankPtr + k]
-      // So bankValid[n] == validVec[n - deqBankPtr]
-      val validIdx = Mux(idx.asUInt >= deqBankPtr.value,
-        idx.asUInt - deqBankPtr.value,
-        ((idx + IBufNBank).asUInt - deqBankPtr.value)(log2Ceil(IBufNBank) - 1, 0)
-      )
-      val bankAdvance = Mux(validIdx >= DecodeWidth.U,
-        false.B,
-        validVec(validIdx(log2Ceil(DecodeWidth) - 1, 0))
-      ) && io.out.head.ready
-      ptr := Mux(bankAdvance , ptr + 1.U, ptr)
-    }
+  val nextStepData = Wire(Vec(2 * DecodeWidth, new IBufEntry))
+  val ptrMatch = new QPtrMatchMatrix(deqPtrVec, enqPtrVec)
+  for (i <- 0 until 2 * DecodeWidth) {
+    val enqMatchVec = VecInit(ptrMatch(i))
+    val enqBypassEnVec = io.in.bits.valid.asBools.zip(enqOffset).map{ case (v, o) => v && enqMatchVec(o) }
+    val enqBypassEn = io.in.fire && VecInit(enqBypassEnVec).asUInt.orR
+    val enqBypassData = Mux1H(enqBypassEnVec, enqData)
+    val readData = if (i < DecodeWidth) deqData(i) else ibuf.io.rdata(i)
+    nextStepData(i) := Mux(enqBypassEn, enqBypassData, readData)
   }
+  val deqEnable_n = io.out.map(o => !o.fire) :+ true.B
+  for (i <- 0 until DecodeWidth) {
+    deqData(i) := ParallelPriorityMux(deqEnable_n, nextStepData.drop(i).take(DecodeWidth + 1))
+  }
+
+  ibuf.io.raddr := VecInit(deqPtrVecNext.map(_.value))
 
   // Flush
   when (io.flush) {
     allowEnq := true.B
-    enqPtrVec := enqPtrVec.indices.map(_.U.asTypeOf(new IBufPtr))
-    deqBankPtrVec := deqBankPtrVec.indices.map(_.U.asTypeOf(new IBufBankPtr))
-    deqInBankPtr := VecInit.fill(IBufNBank)(0.U.asTypeOf(new IBufInBankPtr))
-    deqPtr := 0.U.asTypeOf(new IBufPtr())
-    outputEntries.foreach(_.valid := false.B)
+    deqPtrVec := deqPtrVec.indices.map(_.U.asTypeOf(new IbufPtr))
+    enqPtrVec := enqPtrVec.indices.map(_.U.asTypeOf(new IbufPtr))
   }
   io.full := !allowEnq
 
@@ -343,3 +222,4 @@ class IBuffer(implicit p: Parameters) extends XSModule with HasCircularQueuePtrH
   )
   generatePerfEvent()
 }
+
