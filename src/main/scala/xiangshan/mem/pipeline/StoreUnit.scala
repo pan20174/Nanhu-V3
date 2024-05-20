703da24e79043c55c016487bf1bc3e8961c989dd
linmingfeng <1550263950@qq.com>
2024-05-20 23:13:03 +0800

    rebuild StoreUnit

--- src/main/scala/xiangshan/mem/pipeline/StoreUnit.scala
+++ src/main/scala/xiangshan/mem/pipeline/StoreUnit.scala
@@ -280,10 +280,83 @@ class StoreUnit(implicit p: Parameters) extends XSModule with HasPerfLogging {
   s0_out.bits.uop.cf.exceptionVec(storeAddrMisaligned) := !s0_addrAligned && s0_in.bits.uop.loadStoreEnable
   s0_out.bits.uop.cf.exceptionVec(storePageFault) := Mux(s0_in.bits.uop.loadStoreEnable & io.vmEnable, illegalAddr, false.B)
 
+//  val store_s1 = Module(new StoreUnit_S1)
+  val s1_in = Wire(Decoupled(new LsPipelineBundle))
+  val s1_out = Wire(Decoupled(new LsPipelineBundle))
+  val s1_enableMem = s1_in.bits.uop.loadStoreEnable
 
-  val store_s1 = Module(new StoreUnit_S1)
-  val store_s2 = Module(new StoreUnit_S2)
-  val store_s3 = Module(new StoreUnit_S3)
+  // mmio cbo decoder
+  val s1_is_mmio_cbo = s1_in.bits.uop.ctrl.fuOpType === LSUOpType.cbo_clean ||
+    s1_in.bits.uop.ctrl.fuOpType === LSUOpType.cbo_flush ||
+    s1_in.bits.uop.ctrl.fuOpType === LSUOpType.cbo_inval
+
+  val s1_paddr = io.tlb.resp.bits.paddr(0)
+  val s1_tlb_miss = io.tlb.resp.bits.miss && s1_enableMem
+  val s1_mmio = s1_is_mmio_cbo
+  val s1_exception = Mux(s1_enableMem, ExceptionNO.selectByFu(s1_out.bits.uop.cf.exceptionVec, staCfg).asUInt.orR, false.B)
+
+  //FDI check
+  io.fdiReq.valid := s1_out.fire //TODO: temporarily assignment
+  io.fdiReq.bits.addr := s1_out.bits.vaddr //TODO: need for alignment?
+  io.fdiReq.bits.inUntrustedZone := s1_out.bits.uop.fdiUntrusted
+  io.fdiReq.bits.operation := FDIOp.write
+
+  s1_in.ready := true.B
+  io.tlb.resp.ready := true.B
+
+  val s1_rsFeedback = Wire(ValidIO(new RSFeedback))
+  s1_rsFeedback.valid := s1_in.valid && s1_tlb_miss
+  s1_rsFeedback.bits.flushState := io.tlb.resp.bits.ptwBack
+  s1_rsFeedback.bits.rsIdx := s1_in.bits.rsIdx
+  s1_rsFeedback.bits.sourceType := RSFeedbackType.tlbMiss
+  XSDebug(s1_rsFeedback.valid,
+    "S1 Store: tlbHit: %d rsBank: %d rsIdx: %d\n",
+    s1_in.valid && !s1_tlb_miss,
+    s1_rsFeedback.bits.rsIdx.bankIdxOH,
+    s1_rsFeedback.bits.rsIdx.entryIdxOH
+  )
+
+  // get paddr from dtlb, check if rollback is needed
+  // writeback store inst to lsq
+  s1_out.valid := s1_in.valid && !s1_tlb_miss
+  s1_out.bits := s1_in.bits
+  s1_out.bits.paddr := s1_paddr
+  s1_out.bits.miss := false.B
+  s1_out.bits.mmio := s1_mmio && s1_enableMem
+  s1_out.bits.uop.cf.exceptionVec(storePageFault) := (io.tlb.resp.bits.excp(0).pf.st || s1_in.bits.uop.cf.exceptionVec(storePageFault)) && s1_enableMem
+  s1_out.bits.uop.cf.exceptionVec(storeAccessFault) := io.tlb.resp.bits.excp(0).af.st && s1_enableMem
+
+  io.lsq.valid := s1_in.valid
+  io.lsq.bits := s1_out.bits
+  io.lsq.bits.miss := s1_tlb_miss
+
+
+//  val store_s2 = Module(new StoreUnit_S2)
+  val s2_in = Wire(Decoupled(new LsPipelineBundle))
+  val s2_out = Wire(Decoupled(new LsPipelineBundle))
+
+  val s2_enableMem = s2_in.bits.uop.loadStoreEnable
+  val s2_pmp = WireInit(io.pmp)
+  val s2_static_pm = RegEnable(io.tlb.resp.bits.static_pm,io.tlb.resp.valid)
+  when(s2_static_pm.valid) {
+    s2_pmp.ld := false.B
+    s2_pmp.st := false.B
+    s2_pmp.instr := false.B
+    s2_pmp.mmio := s2_static_pm.bits
+  }
+
+  val s2_exception = ExceptionNO.selectByFu(s2_out.bits.uop.cf.exceptionVec, staCfg).asUInt.orR && s2_enableMem
+  val s2_is_mmio = (s2_in.bits.mmio || s2_pmp.mmio) && s2_enableMem
+
+  s2_in.ready := true.B
+  s2_out.bits := s2_in.bits
+  s2_out.bits.mmio := s2_is_mmio && !s2_exception
+  s2_out.bits.uop.cf.exceptionVec(storeAccessFault) := (s2_in.bits.uop.cf.exceptionVec(storeAccessFault) || s2_pmp.st) && s2_enableMem
+  s2_out.valid := s2_in.valid && (!s2_is_mmio || s2_exception)
+  //FDI store access fault
+  s2_out.bits.uop.cf.exceptionVec(fdiUStoreAccessFault) := io.fdiResp.fdi_fault === FDICheckFault.UWriteFDIFault
+
+//  val store_s3 = Module(new StoreUnit_S3)
 
 //  store_s0.io.in <> io.stin
 //  store_s0.io.dtlbReq <> io.tlb.req
@@ -291,33 +364,48 @@ class StoreUnit(implicit p: Parameters) extends XSModule with HasPerfLogging {
 //  store_s0.io.rsIdx := io.rsIdx
 //  store_s0.io.vmEnable := io.vmEnable
 
-  io.fdiReq := store_s1.io.fdiReq
-  store_s2.io.fdiResp := io.fdiResp
+//  io.fdiReq := store_s1.io.fdiReq
+//  store_s2.io.fdiResp := io.fdiResp
 
   io.storeMaskOut.valid := s0_in.valid
   io.storeMaskOut.bits.mask := s0_out.bits.mask
   io.storeMaskOut.bits.sqIdx := s0_out.bits.uop.sqIdx
 
-  PipelineConnect(s0_out, store_s1.io.in, true.B, s0_out.bits.uop.robIdx.needFlush(io.redirect_dup(0)))
+  PipelineConnect(s0_out, s1_in, true.B, s0_out.bits.uop.robIdx.needFlush(io.redirect_dup(0)))
 
 
-  store_s1.io.dtlbResp <> io.tlb.resp
-  io.lsq <> store_s1.io.lsq
+//  store_s1.io.dtlbResp <> io.tlb.resp
+//  io.lsq <> store_s1.io.lsq
 
-  PipelineConnect(store_s1.io.out, store_s2.io.in, true.B, store_s1.io.out.bits.uop.robIdx.needFlush(io.redirect_dup(1)))
+  PipelineConnect(s1_out, s2_in, true.B, s1_out.bits.uop.robIdx.needFlush(io.redirect_dup(1)))
 
   // feedback tlb miss to RS in store_s2
-  io.feedbackSlow := Pipe(store_s1.io.rsFeedback)
+  io.feedbackSlow := Pipe(s1_rsFeedback)
 
-  store_s2.io.pmpResp <> io.pmp
+//  store_s2.io.pmpResp <> io.pmp
 //  store_s2.io.static_pm := RegNext(io.tlb.resp.bits.static_pm)
-  store_s2.io.static_pm := RegEnable(io.tlb.resp.bits.static_pm,io.tlb.resp.valid)
-  io.lsq_replenish := store_s2.io.out.bits // mmio and exception
-  PipelineConnect(store_s2.io.out, store_s3.io.in, true.B, store_s2.io.out.bits.uop.robIdx.needFlush(io.redirect_dup(2)))
+//  store_s2.io.static_pm := RegEnable(io.tlb.resp.bits.static_pm,io.tlb.resp.valid)
+  io.lsq_replenish := s2_out.bits // mmio and exception
 
-  store_s3.io.stout <> io.stout
-  store_s3.io.redirect := io.redirect
+  val s3_in = Wire(Decoupled(new LsPipelineBundle))
+  val s3_out = Wire(Decoupled(new ExuOutput))
+  s3_in.ready := true.B
 
+  s3_out := DontCare
+  s3_out.valid := s3_in.valid && !s3_in.bits.uop.robIdx.needFlush(io.redirect)
+  s3_out.bits.uop := s3_in.bits.uop
+  s3_out.bits.data := DontCare
+  s3_out.bits.redirectValid := false.B
+  s3_out.bits.redirect := DontCare
+  s3_out.bits.debug.isMMIO := s3_in.bits.mmio
+  s3_out.bits.debug.paddr := s3_in.bits.paddr
+  s3_out.bits.debug.vaddr := s3_in.bits.vaddr
+  s3_out.bits.debug.isPerfCnt := false.B
+  s3_out.bits.fflags := DontCare
+
+  io.stout <> s3_out
+
+  PipelineConnect(s2_out, s3_in, true.B, s2_out.bits.uop.robIdx.needFlush(io.redirect_dup(2)))
   private def printPipeLine(pipeline: LsPipelineBundle, cond: Bool, name: String): Unit = {
     XSDebug(cond,
       p"$name" + p" pc ${Hexadecimal(pipeline.uop.cf.pc)} " +
@@ -329,5 +417,5 @@ class StoreUnit(implicit p: Parameters) extends XSModule with HasPerfLogging {
   }
 
   printPipeLine(s0_out.bits, s0_out.valid, "S0")
-  printPipeLine(store_s1.io.out.bits, store_s1.io.out.valid, "S1")
+  printPipeLine(s1_out.bits, s1_out.valid, "S1")
 }
